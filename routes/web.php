5b619bc97944b72b27afd3ca0501f17395e4c2c8
olakehinde <olakehinde59@gmail.com>
2018-09-27 13:24:25 +0100

    added more routes for the resource methods

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('companies', 'CompaniesController');
+Route::resource('projects', 'ProjectsController');
+Route::resource('roles', 'RolesController');
+Route::resource('task', 'TasksController');
+Route::resource('users', 'UsersController');
+Route::resource('companies', 'CompaniesController');
