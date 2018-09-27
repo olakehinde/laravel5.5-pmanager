642bfa05a4bb91658317367b477cce14bb3b4aaa
olakehinde <olakehinde59@gmail.com>
2018-09-27 20:39:13 +0100

    fixed the undefined session variable error on the success flash message page

--- resources/views/partials/success.blade.php
+++ resources/views/partials/success.blade.php
@@ -1,8 +1,8 @@
-@if($session()->has('success'))
+@if(session()->has('success'))
 	<div class="alert alert-dismissible alert-danger">
 		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 			<span aria-hidden="true">&times;</span>
 		</button>
-		<strong>{!! $session()->get('success') !!}</strong>
+		<strong>{!! session()->get('success') !!}</strong>
 	</div>
 @endif
