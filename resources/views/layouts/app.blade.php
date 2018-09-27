3f0da1a63895fadfa60251af07cb1d2ab4565944
olakehinde <olakehinde59@gmail.com>
2018-09-27 15:25:49 +0100

    included the errors and success flash page on the main app layout page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,6 +72,8 @@
         </nav>
 
         <div class="container">
+            @include('partials.errors')
+            @include('partials.success')
             <div class="row">
                 @yield('content')
             </div>
