0e975d538b009a8551c9dcf8f21091113f39774d
olakehinde <olakehinde59@gmail.com>
2018-09-27 15:23:44 +0100

    added an errors page to flash session error message

---
+++ resources/views/partials/errors.blade.php
@@ -0,0 +1,11 @@
+@if (isset($errors) && count($errors) > 0)
+	<div class="alert alert-dismissible alert-danger fade show">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+		</button>
+
+		@foreach($errors->all() as $error)
+			<li><strong>{!! $error !!}</strong></li>
+		@endforeach
+	</div>
+@endif
