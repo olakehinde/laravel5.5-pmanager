2e4d05fda4c51c196f4232f205537d3355648dcd
olakehinde <olakehinde59@gmail.com>
2018-09-27 12:31:23 +0100

    created view files for index, show, edit and create method

---
+++ resources/views/companies/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('content')
+	<div class="panel panel-default">
+		<div class="panel-heading">HEaading</div>
+		<div class="panel-body">
+			<ul class="list-group">
+				<li class="list-group-item">item 1</li>
+				<li class="list-group-item">item 2</li>
+				<li class="list-group-item">item 3</li>
+			</ul>
+		</div>
+	</div>
+@endsection
