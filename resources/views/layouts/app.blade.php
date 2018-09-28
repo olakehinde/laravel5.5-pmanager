3bb8b1a8956b193a8e3a558ce5cb57ee05eca120
olakehinde <olakehinde59@gmail.com>
2018-09-28 14:42:41 +0100

    added a notification menu item to the app layout page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,10 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li><a href="{{ route('companies.index') }}">My Companies</a></li>
+                            <li><a href="{{ route('projects.index') }}">Projects</a></li>
+                            <li><a href="{{ route('tasks.index') }}">Tasks</a></li>
+
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
