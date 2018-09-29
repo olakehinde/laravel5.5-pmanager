28109cbbd981f775f62ecab2716b9d45de01de9a
olakehinde <olakehinde59@gmail.com>
2018-09-29 05:58:53 +0100

    fixed the typo error in project controller

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -4,17 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProjectsController extends Controller
 {
-    /**
+   /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        if (Auth::check()) {
+            $projects = Project::where('user_id', Auth::user()->id)->get();
+
+            return view('projects.index', ['projects' => $projects]);
+        }
+
+        return view('auth.login');
+        
     }
 
     /**
@@ -22,9 +30,9 @@ class ProjectsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create($company_id = null)
     {
-        //
+        return view('projects.create', ['company_id' => $company_id]);
     }
 
     /**
@@ -35,51 +43,82 @@ class ProjectsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if (Auth::check()) {
+            $project = Project::create([
+                'name' => $request->input('name'),
+                'description' => $request->input('description'),
+                'company_id' => $request->input('company_id'),
+                'user_id' => Auth::user()->id
+            ]);
+
+            if ($project) {
+                return redirect()->route('projects.show', ['project' => $project->id])->with('success', 'Project created Successfully');
+            }
+            return back()->WithInput()->with('errors', 'Sorry, Project cannot be created. Try again');
+        }
+
+        return back()->WithInput()->with('errors', 'Sorry, you do not have Permission to create a Project. Please Login to create Project');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Project  $project
+     * @param  \App\Models\Project  $Project
      * @return \Illuminate\Http\Response
      */
     public function show(Project $project)
     {
-        //
+        $project = Project::find($project->id); 
+        return view('projects.show', ['project' => $project]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Project  $project
+     * @param  \App\Models\Project  $Project
      * @return \Illuminate\Http\Response
      */
     public function edit(Project $project)
     {
-        //
+        $project = Project::find($project->id);
+        return view('projects.edit', ['project' => $project]);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Project  $project
+     * @param  \App\Models\Project  $Project
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Project $project)
     {
-        //
+        $updateProject = Project::find($project->id)->update([
+            'name' => $request->input('name'),
+            'description' => $request->input('description')
+        ]);
+
+        if ($updateProject) {
+            return redirect()->route('projects.show', ['project' => $project->id])->with('success', 'Project updated Successfully');
+        }
+        return back()->WithInput();
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Project  $project
+     * @param  \App\Models\Project  $Project
      * @return \Illuminate\Http\Response
      */
     public function destroy(Project $project)
     {
-        //
+        $findProject = Project::find($project->id);
+
+        if ($findProject->delete()) {
+            return redirect()->route('projects.index')
+                             ->with('success', 'Project deleted Successfully');
+        }
+
+        return back()->WithInput()->with('error', 'Project cannot be deleted');
     }
 }
