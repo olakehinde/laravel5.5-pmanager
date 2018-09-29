908a2ef9674f2ca7e737acd60ec4d5de668e6336
olakehinde <olakehinde59@gmail.com>
2018-09-29 05:54:42 +0100

    grouped routes in the auth middleware so that only logged in users can access them

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,14 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('companies', 'CompaniesController');
-Route::resource('projects', 'ProjectsController');
-Route::resource('roles', 'RolesController');
-Route::resource('tasks', 'TasksController');
-Route::resource('users', 'UsersController');
+Route::middleware(['auth'])->group(function() {
+
+	Route::resource('companies', 'CompaniesController');
+	Route::get('projects/create/{company_id?}', 'ProjectsController@create');
+	Route::resource('projects', 'ProjectsController');
+	Route::resource('roles', 'RolesController');
+	Route::resource('tasks', 'TasksController');
+	Route::resource('users', 'UsersController');
+	
+});
+
