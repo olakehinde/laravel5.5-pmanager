be5d0016700c99d42b8375eb4dfaccd7cd66e004
olakehinde <olakehinde59@gmail.com>
2018-10-01 06:17:35 +0100

    added a method to fetch user in the comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -17,4 +17,8 @@ class Comment extends Model
     public function commentable() {
     	return $this->morphTo();
     }
+
+    public function user() {
+        return $this->hasOne('App\Models\User', 'id', 'user_id');
+    }    
 }
