f56a52e0b28f9c23e8283b8bff138c52a7876dc9
olakehinde <olakehinde59@gmail.com>
2018-10-01 06:43:51 +0100

    made minor adjustments in the project controller to accomodate the code refactor done in the show.blade file and the comment section that was moved to a separate file in the partials folder

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -74,7 +74,9 @@ class ProjectsController extends Controller
     public function show(Project $project)
     {
         $project = Project::find($project->id); 
-        return view('projects.show', ['project' => $project]);
+        $comments = $project->comments;
+        
+        return view('projects.show', ['project' => $project, 'comments' => $comments]);
     }
 
     /**
