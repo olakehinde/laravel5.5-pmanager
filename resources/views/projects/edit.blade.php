cf7b98c4a45ec99bd665185dd7f25eadb25b35aa
olakehinde <olakehinde59@gmail.com>
2019-02-05 08:29:54 +0100

    added proper styling and icons to the project edit page

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -2,18 +2,19 @@
 
 @section('content')
 	<div class="row col-md-9 col-lg-9 col-sm-9 pull-left">
-		<div class="row col-sm-12 col-md-12 col-lg-12" style="background-color: white; margin: 3px;">
+		<div class="row col-sm-12 col-md-12 col-lg-12" style="background-color: white; border-radius: 5px;">
 
-			<form method="post" action="{{ route('projects.update', [$project->id]) }}" class="col-sm-12">
+			<form method="post" action="{{ route('projects.update', [$project->id]) }}" class="col-sm-12" style="margin-top: 10px;">
 				{{ csrf_field() }}
 
 
 				<div class="form-group" style="padding-top: 2px; padding-bottom: 2px;">
-					<label for="project-name">Name <span class="required">*</span></span></label>
+					<label for="project-name" style="margin-bottom: 3px;">Name <span class="required">*</span></label>
 					<input 
 						type="text" 
 						name="name" 
 						id="project-name"
+						class="form-control" 
 						required="true"
 						spellcheck="false" 
 						placeholder="Enter project name"
@@ -22,7 +23,7 @@
 				</div>
 
 				<div class="form-group">
-					<label for="project-description">Description</span></label>
+					<label for="project-description" style="margin-bottom: 3px;">Description</span></label>
 					<textarea
 						name="description"
 						id="project-description"
@@ -37,7 +38,7 @@
 
 				<div class="form-group">
 					<input type="submit" class="btn btn-success" value="Save">
-					<a href="/projects/{{$project->id}}" class="btn btn-danger">Cancel</a>
+					<a href="/projects/{{$project->id}}" class="btn btn-danger"><i class="fa fa-close"></i> Cancel</a>
 				</div>
 				<input type="hidden" name="_method" value="put">
 			</form>
@@ -48,8 +49,8 @@
 		<div class="sidebar-module">
 			<h4>Action</h4>
 			<ol class="list-unstyled">
-				<li style="padding-bottom: 3px"><a href="/projects/{{ $project->id }}" class="btn btn-primary">View Project</a></li>
-				<li style="padding-bottom: 3px"><a href="/projects" class="btn btn-danger">View all Projects</a></li>
+				<li style="padding-bottom: 3px"><a href="/projects/{{ $project->id }}" class="btn btn-primary"><i class="fa fa-eye"></i> View Project</a></li>
+				<li style="padding-bottom: 3px"><a href="/projects" class="btn btn-danger"><i class="fa fa-list"></i> View all Projects</a></li>
 				
 			</ol>
 		</div>
