c88c09dc5082871ad854217bcdfc11f9687db6cd
olakehinde <olakehinde59@gmail.com>
2019-02-06 06:23:25 +0100

    cleaned up project index page. deleted the commented code

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,19 +1,6 @@
 @extends('layouts.app')
 @section('content')
-	{{--<div class="col-md-6-offset-3 col-lg-6 col-lg-offest-3 col-md-offset-3">
-		<div class="panel panel-primary">
-			<div class="panel-heading panel-primary">Project <a class="btn btn-success pull-right btn-sm" href="/projects/create">Add new Project</a></div>
-			<div class="panel-body">
-				<ul class="list-group">
-					@foreach($projects as $project)
-					<li class="list-group-item"><a href="/projects/{{$project->id}}">{{ $project->name }}</a></li>
-					@endforeach
-				</ul>
-			</div>
-		</div>
-	</div>--}}
-
-	<div class="container mt-5">
+    <div class="container mt-5">
 		<div class="row pb-1">
 			<div class="col">My Projects</div>
 			<div class="col"><a class="btn btn-success pull-right btn-sm" href="/projects/create"><i class="fa fa-plus"></i> Add new Project</a></div>
