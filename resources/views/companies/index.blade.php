b4b6ece95d738f42242e1dcbc9227f82df0ab598
olakehinde <olakehinde59@gmail.com>
2019-02-06 07:54:11 +0100

    integrated new design for the company index page

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-	<div class="col-md-6-offset-3 col-lg-6 col-lg-offest-3 col-md-offset-3">
+	{{-- <div class="col-md-6-offset-3 col-lg-6 col-lg-offest-3 col-md-offset-3">
 		<div class="panel panel-primary">
 			<div class="panel-heading panel-primary">Company <a class="btn btn-success pull-right btn-sm" href="/companies/create">Add new Company</a></div>
 			<div class="panel-body">
@@ -11,5 +11,28 @@
 				</ul>
 			</div>
 		</div>
+	</div> --}}
+
+<div class="container mt-5">
+	<div class="row pb-1">
+		<div class="col text-white">My Company</div>
+		<div class="col"><a class="btn btn-success pull-right btn-sm" href="/companies/create"><i class="fa fa-plus"></i> Add new Company</a></div>
 	</div>
+    @if(!$companies)
+    	<p>You have not created any Company.</p>
+    @endif
+
+    <div class="card-deck">
+    	@foreach($companies as $company)
+        <div class="card mx-1">
+        	<a href="/companies/{{$company->id}}">
+	            <div class="card-body px-2">
+	                <h6 class="card-title">{{ $company->name }}</h6>
+	                <p class="card-text" style="font-size:15px">{{ $company->description }}</p>
+	            </div>
+        	</a>
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
